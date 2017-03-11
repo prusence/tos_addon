2198b8fb39a7db9a9bb4ec92e72532a5698332ed
Puryu <prusence@gmail.com>
2017-03-11 09:18:41 +0900

    make command
    /chbt [num] [title] [message]のコマンド作成

--- chatbtn/src/addon_d/chatbtn/chatbtn.lua
+++ chatbtn/src/addon_d/chatbtn/chatbtn.lua
@@ -79,54 +79,58 @@ function CHATBTN_CREATE_BUTTONS()
         chatbutton[i]:SetEventScript(ui.LBUTTONUP, "CHATBTN_ON_CLICK("..i..")");
         chatbutton[i]:SetClickSound('button_click_big');
         chatbutton[i]:SetOverSound('button_over');
-        chatbutton[i]:SetTextTooltip("{s14}"..UNESCAPE(g["settings"]["button"..i]["msg"]));
+        chatbutton[i]:SetTextTooltip("{s14}"..CHATBTN_UNESCAPE(g["settings"]["button"..i]["msg"]));
         chatbutton[i]:ShowWindow(1);
     end
 end
 
 function CHATBTN_ON_CLICK(num)
-    ui.Chat(UNESCAPE(g["settings"]["button"..num]["msg"]));
+    ui.Chat(CHATBTN_UNESCAPE(g["settings"]["button"..num]["msg"]));
 end
 
 function CHATBTN_COMMAND(command)
     local cmd = table.remove(command, 1);
     if not cmd then
+        CHAT_SYSTEM("/chbt [num] [title] [message]")
         CHAT_SYSTEM("/chbt msg [num] [message]");
         CHAT_SYSTEM("/chbt title [num] [title]");
         CHAT_SYSTEM("/chbt size [num]");
         CHAT_SYSTEM("/chbt count [num]");
     elseif cmd == 'msg' then
+        -- /chbt msg [num] [message]
         local msg = "";
-        cmd = table.remove(command, 1);
+        num = table.remove(command, 1);
         for index, item in ipairs(command) do
             msg = msg..item.." ";
         end
         msg = string.sub(msg, 1, #msg - 1);
 
         for i = 1, g.settings.count do
-            if cmd == i.."" then
+            if num == i.."" then
                 CHATBTN_SET_MSG(i, msg);
                 break;
             end
         end
     elseif cmd == 'title' then
+        -- /chbt title [num] [title]
         local title = "";
-        cmd = table.remove(command, 1);
+        num = table.remove(command, 1);
         title = table.remove(command, 1);
 
         for i = 1, g.settings.count do
-            if cmd == i.."" then
+            if num == i.."" then
                 CHATBTN_SET_TITLE(i, title);
                 break;
             end
         end
     elseif cmd == 'size' then
+        -- /chbt size [num]
         local size = tonumber(table.remove(command, 1));
         if size == nil or size <= 0 then
             CHAT_SYSTEM("Please Input Number!");
             return;
         else
-            if CHECK_WIDE(g.settings.count, size) == false then
+            if CHATBTN_CHECK_WIDE(g.settings.count, size) == false then
                 return;
             end
             g.settings.size = size;
@@ -134,12 +138,13 @@ function CHATBTN_COMMAND(command)
             CHATBTN_CREATE_BUTTONS();
         end
     elseif cmd == 'count' then
+        -- /chbt count [num]
         local count = tonumber(table.remove(command, 1));
         if count == nil or count < 0 then
             CHAT_SYSTEM("Please Input Number!");
             return;
         else
-            if CHECK_WIDE(count, g.settings.size) == false then
+            if CHATBTN_CHECK_WIDE(count, g.settings.size) == false then
                 return;
             end
             for i = count + 1, g.settings.count do
@@ -148,6 +153,24 @@ function CHATBTN_COMMAND(command)
             g.settings.count = count;
             CHAT_SYSTEM("Set the button count to "..g.settings.count);
             CHATBTN_CREATE_BUTTONS();
+    else
+        -- /chbt [num] [title] [message]
+        for i = 1, g.settings.count do
+            if cmd == i.."" then
+                -- タイトルの取得
+                title = table.remove(command, 1);
+                
+                -- メッセージの取得
+                local msg = "";
+                for index, item in ipairs(command) do
+                    msg = msg..item.." ";
+                end
+                msg = string.sub(msg, 1, #msg - 1);
+
+                -- セット
+                CHATBTN_SET_TITLE(i, title);
+                CHATBTN_SET_MSG(i, msg);
+            end
         end
     end
     acutil.saveJSON(g.settingFileLoc, g.settings);
@@ -156,7 +179,7 @@ end
 function CHATBTN_SET_MSG(num, msg)
     chatbutton[num]:SetTextTooltip("{s14}"..msg);
     CHAT_SYSTEM("Set the button"..num.." message to \'"..msg.."\'");
-    g["settings"]["button"..num]["msg"] = ESCAPE(msg);
+    g["settings"]["button"..num]["msg"] = CHATBTN_ESCAPE(msg);
 end
 
 function CHATBTN_SET_TITLE(num, title)
@@ -165,7 +188,7 @@ function CHATBTN_SET_TITLE(num, title)
     CHAT_SYSTEM("Set the button"..num.." title to \'"..title.."\'");
 end
 
-function CHECK_WIDE(count, size)
+function CHATBTN_CHECK_WIDE(count, size)
     if (size - 3) * count + 3 >= 500 then
         CHAT_SYSTEM("Width will over the range!");
         return false;
@@ -174,7 +197,7 @@ function CHECK_WIDE(count, size)
     end
 end
 
-function ESCAPE(string)
+function CHATBTN_ESCAPE(string)
     if string.sub(string, 1, 1) == "/" then
         string = "$"..string.sub(string, 2, #string);
     end
@@ -182,7 +205,7 @@ function ESCAPE(string)
     return string;
 end
 
-function UNESCAPE(string)
+function CHATBTN_UNESCAPE(string)
     if string.sub(string, 1, 1) == "$" then
         string = "/"..string.sub(string, 2, #string);
     end
